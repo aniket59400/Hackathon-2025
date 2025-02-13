c037fb0852bc7769eb4726d61c82262701a205ec
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2025-02-13 14:53:04 +0530

    min pn push kela

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import NumberFile from './components/NumberFile'
 import NavBar from './components/NavBar'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
@@ -11,6 +12,15 @@ function App() {
    <NavBar />
    
    <NumberFile />
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="history" element={<History/>} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
    </>
   )
 }
---
+++ frontend/src/components/NoPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NoPage() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <div>
+             
+            <p>404 Not Found </p>
+            <p>Please return </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default NoPage;
