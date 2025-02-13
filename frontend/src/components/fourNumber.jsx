65a544e7ee42f410eef0c5bd72350990560d6ed8
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2025-02-13 09:57:34 +0530

    file created

---
+++ frontend/src/components/fourNumber.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function fourNumber() {
+  return (
+    <div>
+      four fourNumber
+    </div>
+  )
+}
+
+export default fourNumber
---
+++ frontend/src/components/numberFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function numberFile() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default numberFile
